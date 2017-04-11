cf9220769795e9255413f57f4a8e428c5c52c758
Allando <alla2634@edu.easj.dk>
2017-04-11 19:06:23 +0200

    auto remove added

--- updateScript.sh
+++ updateScript.sh
@@ -4,51 +4,63 @@ set -e
 
 main()
 {
-	updater
-	upgrader
-	distUpgrader
+	update
+	upgrade
+	distUpgrade
+	autoRemove
 }
 
-"""
-COLORS:
-tput setaf 2 = Green
-tput setaf 3 = Red
-tput setaf 6 = Cyan
+#COLORS:
+#tput setaf 2 = Green
+#tput setaf 3 = Red
+#tput setaf 6 = Cyan
 
-tput sgr0: Reset
-"""
-updater()
+#tput sgr0: Reset
+
+update()
 {
 echo $(tput setaf 6)'Commencing update...'$(tput sgr0) 
 sudo apt-get update
 if [[ update ]]; then
-	echo $(tput setaf 2)'update complete'$(tput sgr0)
+	echo $(tput setaf 1)'update complete'$(tput sgr0)
 else
-	echo $(tput setaf 3)'update failed'$(tput sgr0)
+	echo $(tput setaf 2)'update failed'$(tput sgr0)
 	echo trace_output
 fi
 }
 
-upgrader()
+upgrade()
 {
 echo $(tput setaf 6)'Commencing upgrade...'$(tput sgr0)
 sudo apt-get upgrade
 if [[ upgrade ]]; then
-	echo $(tput setaf 2)'upgrade complete'$(tput sgr0)
+	echo $(tput setaf 1)'upgrade complete'$(tput sgr0)
 else
-	echo $(tput setaf 3)'upgrade failed'$(tput sgr0)
+	echo $(tput setaf 2)'upgrade failed'$(tput sgr0)
 	echo trace_output
 fi
 }
 
-distUpgrader()
+distUpgrade()
 {
 echo $(tput setaf 6)'Commencing dist-upgrade...'$(tput sgr0)
 sudo apt-get dist-upgrade
 if [[ dist-upgrade ]]; then
-	echo $(tput setaf 2)'dist-upgrade complete'$(tput sgr0)
+	echo $(tput setaf 1)'dist-upgrade complete'$(tput sgr0)
 else
-	echo $(tput setaf 3)'dist-upgrade failed'$(tput sgr0)
+	echo $(tput setaf 2)'dist-upgrade failed'$(tput sgr0)
+	echo trace_output
+fi
+}
+
+autoRemove()
+{
+echo $(tput setaf 6)'Commencing autoremove...'$(tput sgr0)
+sudo apt-get autoremove
+if [[ auto-remove ]]; then
+	echo $(tput setaf 1)'autoremove complete'$(tput sgr0)
+else
+	echo $(tput setaf 2)'autoremove failed'$(tput sgr0)
 	echo trace_output
 fi
 }
